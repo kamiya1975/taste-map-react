860e364d7e0222d8e7529df3069a8f9d45ef99e2
kamiya <kamiya@kamiyatoyoakinoMac-mini.local>
2025-06-09 16:53:24 +0900

    App.js MapPageを表示するよう修正

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import './App.css';
-import MapPage from './MapPage';  // これ OK！
+import MapPage from './MapPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Taste MAP アプリ</h1>
-      </header>
-      <main>
-        <MapPage />   {/* ← ここ！！ */}
-      </main>
+    <div>
+      <MapPage />
     </div>
   );
 }
