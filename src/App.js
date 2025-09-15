9d51bf55fbc91b8de2e4c3ac60c24f49285568b5
Toyoaki Kamiya <t-kamiya@taste-databank.com>
2025-09-15 10:59:02 +0900

    wip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
 // src/App.js
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import CameraSmokeTest from "./CameraSmokeTest";
 
-import IntroPage from "./IntroPage";
-import StorePage from "./StorePage";
-import SliderPage from "./SliderPage";
-import MapPage from "./MapPage";
-import ProductPage from "./ProductPage";
-import UserTastePage from "./UserTastePage";
-
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<IntroPage />} />
-      <Route path="/store" element={<StorePage />} />
-      <Route path="/slider" element={<SliderPage />} />
-      <Route path="/map" element={<MapPage />} />
-      <Route path="/products/:jan" element={<ProductPage />} />
-      <Route path="/taste-log" element={<UserTastePage />} />
-    </Routes>
-  );
+export default function App() {
+  return <CameraSmokeTest />;
 }
-
-export default App;
