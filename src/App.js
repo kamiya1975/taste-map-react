0d9783df2cbf8326495d58ae1a7e9b4e6195161f
Toyoaki Kamiya <t-kamiya@taste-databank.com>
2025-09-19 11:18:28 +0900

    wip

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import IntroPage from "./IntroPage";
 import StorePage from "./StorePage";
@@ -12,12 +12,26 @@ import UserTastePage from "./UserTastePage";
 export default function App() {
   return (
     <Routes>
+      {/* ランディング */}
       <Route path="/" element={<IntroPage />} />
+
+      {/* 店舗選択 */}
       <Route path="/store" element={<StorePage />} />
+
+      {/* 嗜好スライダー（別ページ） */}
       <Route path="/slider" element={<SliderPage />} />
+
+      {/* 地図ページ（検索/お気に入り/スキャン） */}
       <Route path="/map" element={<MapPage />} />
+
+      {/* 商品詳細（埋め込み用） */}
       <Route path="/products/:jan" element={<ProductPage />} />
+
+      {/* ユーザー評価ログ */}
       <Route path="/taste-log" element={<UserTastePage />} />
+
+      {/* それ以外は地図へリダイレクト */}
+      <Route path="*" element={<Navigate to="/map" replace />} />
     </Routes>
   );
 }
