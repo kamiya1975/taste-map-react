ef15fbd28c1fa5e2a032f722a7982500d867287c
Toyoaki Kamiya <t-kamiya@taste-databank.com>
2025-09-21 10:52:43 +0900

    wip

--- src/MapPage.js
+++ src/MapPage.js
@@ -542,6 +542,7 @@ function MapPage() {
             sendFavoriteToChild(jan, false);
           } catch {}
           setIsFavoriteOpen(false);
+          setIsRatedOpen(true);
         }
 
         // 最新スナップショットを返信（UI同期用）
