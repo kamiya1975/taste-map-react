81a77541ea3b9949c23a2de5d050b2964928fe91
Toyoaki Kamiya <t-kamiya@taste-databank.com>
2025-09-29 11:25:38 +0900

    wip

--- src/pages/IntroPage.jsx
+++ src/pages/IntroPage.jsx
@@ -182,7 +182,7 @@ function slides(
             style={{
               width: "100%",
               maxWidth: 400,
-              margin: "20px auto 0 auto",
+              margin: "10px auto 0 auto",
               paddingBottom: "calc(env(safe-area-inset-bottom) + 80px)",
             }}
           >
