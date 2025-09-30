1a7c17059afa7e0d5f954fa67bf91030e2ab0958
Toyoaki Kamiya <t-kamiya@taste-databank.com>
2025-09-30 09:01:26 +0900

    wip

--- src/pages/IntroPage.jsx
+++ src/pages/IntroPage.jsx
@@ -174,7 +174,7 @@ function slides(
           <HeroImage
             filename="TasteMap.svg"
             alt="TasteMap"
-            maxWidthPct={60}
+            maxWidthPct={55}
             boxHeight="clamp(56px, 9vh, 84px)"
             margin="24px auto 0 auto"
           />
