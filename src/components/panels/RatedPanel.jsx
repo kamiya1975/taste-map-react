886b0aba6ed6907fb1233822db4e0c9fe33a070e
Toyoaki Kamiya <t-kamiya@taste-databank.com>
2025-10-01 10:02:41 +0900

    wip

--- src/components/panels/RatedPanel.jsx
+++ src/components/panels/RatedPanel.jsx
@@ -307,7 +307,7 @@ export default function RatedPanel({
                     showDate
                     dateValue={item.ratedAt}
                     accentColor={typeColor}
-                    extraRight={<CircleRatingDisplay rating={item.rating} size={40} />}
+                    extraRight={<CircleRatingDisplay rating={item.rating} size={30} />}
                   />
                 );
               })}
