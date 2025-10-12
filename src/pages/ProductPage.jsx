435d7ca42bc15de09146166b892c0a1e235cee24
Toyoaki Kamiya <t-kamiya@taste-databank.com>
2025-10-12 11:15:36 +0900

    update

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -536,7 +536,7 @@ export default function ProductPage() {
                 alignSelf: "flex-start",
                 lineHeight: 1,
                 position: "relative",
-                top: "-12px",
+                top: "-11px",
                 marginLeft: "4px" // 横位置を右に動かす
               }}
             >
