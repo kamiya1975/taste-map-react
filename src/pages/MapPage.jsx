d56743fb1860872980ff21508d7bf322e05ce760
Toyoaki Kamiya <t-kamiya@taste-databank.com>
2025-10-26 09:56:11 +0900

    wip

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -1016,7 +1016,7 @@ function MapPage() {
       <ClusterPalettePanel
         isOpen={isClusterOpen}
         onClose={() => setIsClusterOpen(false)}
-        height="calc(20svh - env(safe-area-inset-bottom))" //ドロワー高さ調整
+        height="calc(40svh - env(safe-area-inset-bottom))" //ドロワー高さ調整
       />
 
       {/* 「TasteMapとは？」（PanelShell 版） */}
