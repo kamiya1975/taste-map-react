ca51206104574dfbc88788319053b7d24a63db0c
Toyoaki Kamiya <t-kamiya@taste-databank.com>
2025-11-01 17:29:43 +0900

    wip

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -723,9 +723,8 @@ useEffect(() => {
             }}
           >
             <option value="">基本マップ</option>
-            <option value="PC2">Sweet</option>
-            <option value="PC1">Body</option>
-            <option value="PC3">PC3</option>
+            <option value="PC2">甘味</option>
+            <option value="PC1">ボディ</option>
           </select>
           <span
             aria-hidden="true"
