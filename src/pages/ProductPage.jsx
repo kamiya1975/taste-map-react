a0481d6cd5901bf1acf19c26d8f0f6b0bcce4398
Toyoaki Kamiya <t-kamiya@taste-databank.com>
2025-11-17 11:40:59 +0900

    wip

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -534,8 +534,8 @@ export default function ProductPage() {
             src={`${process.env.PUBLIC_URL || ""}/img/icon cart2.png`}
             alt="cart"
             style={{
-              width: 24,
-              height: 24,
+              width: 30,
+              height: 30,
               objectFit: "contain",
               display: "block",
             }}
