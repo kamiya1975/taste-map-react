a66705408abb08abf1e3f8661a45f54d1b97bf47
神谷豊明 <2024.10.07cultivatejapan@kamiyatoyoakinoMac-mini-3.local>
2025-12-24 10:08:04 +0900

    time

--- src/components/ui/ListRow.jsx
+++ src/components/ui/ListRow.jsx
@@ -36,9 +36,10 @@ export default function ListRow({
       const y = d.getFullYear();
       const m = String(d.getMonth() + 1).padStart(2, "0");
       const day = String(d.getDate()).padStart(2, "0");
-      const hh = String(d.getHours()).padStart(2, "0");
-      const mm = String(d.getMinutes()).padStart(2, "0");
-      return `${y}/${m}/${day} ,${hh}:${mm}`;
+    //  const hh = String(d.getHours()).padStart(2, "0");
+    //  const mm = String(d.getMinutes()).padStart(2, "0");
+    //  return `${y}/${m}/${day} ,${hh}:${mm}`;
+      return `${y}.${m}.${day}`;
     } catch { return "（日時不明）"; }
   };
 
